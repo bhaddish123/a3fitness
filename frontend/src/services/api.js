f6560ff3ec4d3da58598a1748d7b6a8b4168f08d
Bhaddish123 <bh1001025@wcupa.edu>
2026-02-06 13:23:07 -0500

    configuring for db connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8081/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8082/api';
 
 // Create axios instance
 const api = axios.create({
